aeb0f908973093e38855d5cf737fee09b3e70b38
leonardisanchez <140545302+leonardisanchez@users.noreply.github.com>
2024-05-12 22:38:51 -0500

    Add files via upload

---
+++ S2_10_Array1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package S02_10;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class S2_10_Array1 {
+
+    public static void main(String[] args) {
+        // Declarar e inicializar un array de enteros
+        int[] numeros = {1, 2, 3, 4, 5};
+
+        // Iterar sobre el array e imprimir cada elemento
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Elemento " + i + ": " + numeros[i]); // Imprime el índice y el valor del elemento
+        }
+    }
+}
