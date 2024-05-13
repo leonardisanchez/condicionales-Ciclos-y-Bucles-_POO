13661bc5ff35459cec3d90152f8b1cb009073920
leonardisanchez <140545302+leonardisanchez@users.noreply.github.com>
2024-05-12 22:43:12 -0500

    Add files via upload

---
+++ S02_10_While.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package S02_10;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class S02_10_While {
+
+    public static void main(String[] args) {
+        int i = 1;
+
+        // While: Repetir mientras i sea menor o igual que 5
+        while (i <= 5) {
+            System.out.println("El valor de i es: " + i); // Imprime el valor de i
+            i++; // Incrementa el valor de i en 1
+        }
+    }
+}
