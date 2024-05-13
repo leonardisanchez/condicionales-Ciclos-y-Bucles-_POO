0c6e31e37243d2bd0adcd98402d9cd74fdd42e0f
leonardisanchez <140545302+leonardisanchez@users.noreply.github.com>
2024-05-12 22:39:34 -0500

    Add files via upload

---
+++ S02_10_IfElse.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package S02_10;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class S02_10_IfElse {
+    
+    
+    public static void main(String[] args) {
+        int edad = 18;
+
+        // if-else: Verificar si la edad es mayor o igual a 18
+        if (edad >= 18) {
+            System.out.println("Eres mayor de edad"); // Imprime que es mayor de edad
+        } else {
+            System.out.println("Eres menor de edad"); // Imprime que es menor de edad
+        }
+    }
+}
+
+    
+
