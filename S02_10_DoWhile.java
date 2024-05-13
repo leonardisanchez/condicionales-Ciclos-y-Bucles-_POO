8ca43fdcfc2e66055063931a01ad7c2fd654815c
leonardisanchez <140545302+leonardisanchez@users.noreply.github.com>
2024-05-12 22:18:48 -0500

    Add files via upload

---
+++ S02_10_DoWhile.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package S02_10;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class S02_10_DoWhile {
+    
+    
+    public static void main(String[] args) {
+        int i = 1;
+        
+        // Do-While: Ejecutar al menos una vez y repetir mientras i sea menor o igual que 5
+        do {
+            System.out.println("El valor de i es: " + i); // Imprime el valor de i
+            i++; // Incrementa el valor de i en 1
+        } while (i <= 5); // Repite mientras i sea menor o igual que 5
+    }
+}
+
+    
+    
+
