b1b0a70df332cf132707226206893ee450bcb43e
leonardisanchez <140545302+leonardisanchez@users.noreply.github.com>
2024-05-12 22:23:36 -0500

    Add files via upload

---
+++ S2_10_For.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package S02_10;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class S2_10_For {
+
+    public static void main(String[] args) {
+        // For: Iterar de 1 a 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("El valor de i es: " + i); // Imprime el valor de i
+        }
+    }
+}
